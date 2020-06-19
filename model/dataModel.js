4ed82c2442c780ea21810b448593285cb1b28e45
ProjectBackupRahul <projectbackuprahul@gmail.com>
2020-06-19 15:12:43 +0530

    Final Commit

---
+++ model/dataModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const DataSchema = new Schema({
+  transaction_id: {
+    type: String,
+    required: true
+  },
+  openingPrice: {
+    type: Number,
+    required: true
+  },
+  closingPrice: {
+    type: Number,
+    required: true
+  },
+  high: {
+    type: Number,
+    required: true
+  },
+  low: {
+    type: Number,
+    required: true
+  },
+  volume: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = dataModel = mongoose.model("data", DataSchema);
---
+++ server.js
@@ -0,0 +1,60 @@
+const express =  require('express');
+var socket = require('socket.io-client')('wss://ws-feed.zebpay.com/marketdata');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// BodyParser middleware
+// Load User model
+const DataModel = require("./model/dataModel");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+  // DB Config
+  const db = require("./config/keys").mongoURI;
+  // Connect to MongoDB
+  mongoose
+    .connect(db,{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+ socket.on('connect', ()=>{
+    console.log('Socket Connected ----')
+    });
+
+    socket.on('history_singapore/BTC-INR', (data)=>{
+    console.log("Received Data", data);
+
+    //  Data save functionality
+    DataModel.findOne({ transaction_id: data.transaction_id}).then(data => {
+        if (data) {
+          return res.status(400).json({ transaction_id: "This entry is already exists" });
+        } else {
+
+            // @ Dummy Field to save data in Mongo DB 
+          const newData = new DataModel({
+            transaction_id: data.transaction_id,
+            openingPrice: data.opening_price,
+            closingPrice: data.closingPrice,
+            high: data.high,   
+            low: data.low,
+            volume: data.volume
+          });
+              newData
+                .save()
+                .then(user => res.json(data))
+                .catch(err => console.log(err));
+        }
+      });
+    });
+
+    socket.on('disconnect', ()=>{
+    console.log("Socket Disconnected----")
+    });
+
+
+app.listen(PORT, () => {
+console.log(`Listening on port ${PORT}`);
+});
